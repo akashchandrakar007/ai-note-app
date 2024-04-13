c441f05a799006aabf79d2415f401235dede9bdf
Akashdeep Chandrakar <63792903+akashchandrakar@users.noreply.github.com>
2024-04-13 13:00:57 +0530

    Prisma setup done

---
+++ src/lib/db/prims.js
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+global.prismaGlobal = global.prismaGlobal || prismaClientSingleton();
+
+const prisma = global.prismaGlobal;
+
+module.exports = prisma;
+
+if (process.env.NODE_ENV !== "production") global.prismaGlobal = prisma;
