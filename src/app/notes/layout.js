2a15e7b38a02ae2214e4f89a0a37754f26354956
Akashdeep Chandrakar <63792903+akashchandrakar@users.noreply.github.com>
2024-04-13 18:45:33 +0530

    Authenication add with home page

---
+++ src/app/notes/layout.js
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <main className="m-auto max-w-7xl p-4">{children}</main>
+    </>
+  );
+};
+
+export default Layout;
